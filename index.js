5db6a42e7e9ec13498c01c836448ae330de44d09
Vivek Hiray <vivekhiray@Viveks-Laptop.local>
2024-12-14 14:15:54 +0530

    Added level tracking and game start logic also added sound assets

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 var started = false;
+var level = 0;
+
+$(document).keypress(function () {
+    console.log("Key Pressed");
+    if (!started) {
+        $("level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
 
 function nextSequence() {
     userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
+    console.log("Random Number->", randomNumber);
     var randomChoosenColour = buttonColours[randomNumber];
+    console.log("Random Colour->", randomChoosenColour);
     gamePattern.push(randomChoosenColour);
+    console.log("Game Pattern->", gamePattern);
     
     $("#" + randomChoosenColour).fadeOut(100).fadeIn(100);
     playSound(randomChoosenColour);
@@ -15,8 +30,10 @@ function nextSequence() {
 
 $(".btn").click(function () {
     var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
 });
 
 function playSound(name) {
@@ -33,12 +50,14 @@ function animatePress(currentColour) {
 
 function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("Success");
         if (gamePattern.length === userClickedPattern.length) {
             setTimeout(function () {
                 nextSequence();
             }, 1000);
         }
     } else {
+        console.log("Wrong");
         playSound("wrong");
         $("body").addClass("game-over");
         setTimeout(function () {
